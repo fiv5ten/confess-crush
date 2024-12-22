445622c5cb4c86cfaa88543d3326b5f3c51db008
fiv5ten <saya.falah.akbar@gmail.com>
2024-12-23 01:32:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    titleWeb: "A Confession From Me",
+    introTitle: 'Dear Dira',
+    introDesc: `When you're awake, the sun is admiring your eyes
+                When you're asleep, the moon is shining your beauty
+                Every step you take, the grounds blooms a charm
+                And every word you said, the birds hum with harmony`,
+    btnIntro: '^^Ehe^^',
+    title: 'Dira, Will You Be My Girlfriend?',
+    desc: 'I mean we can listen to music together, while eating Mixue under the moonlight hehe :3 ',
+    btnYes: 'Yes, I Will <33',
+    btnNo: 'No, Sorry :3',
+    question: 'You Know That Whatever You Choose, You Will Always Be The Best For Me Right?',
+    btnReply: 'Reply Here <3',
+    reply: 'Yayy, I Hope You Always Happy Dir!',
+    mess: 'I Love You, Dira',
+    messDesc: 'First "Date" on Mixue on Tuesday?',
+    btnAccept: 'Yes Please <3',
+    messLink: 'https://ig.me/m/falahnamaku' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
